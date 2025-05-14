ec2ff54077d38d8e1902ff8c3dded318b8a4f6eb
Valen Torassa <valentintorassacolombero@gmail.com>
2025-05-14 16:00:55 -0300

    final changes

--- API-EnvioMensajes-Telegram.py
+++ API-EnvioMensajes-Telegram.py
@@ -101,8 +101,10 @@ def enviar_mensaje(message):
         return f'Error al enviar mensaje: {response.text}', 500
 
 
-if __name__ == '__main__':
-    app.run(port=5000)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
 
 
 
